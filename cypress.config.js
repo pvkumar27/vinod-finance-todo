a3211b2de1b19a639c1208d52f0d790e3b5b0b27
Vinod <pvkumar27@gmail.com>
2025-08-09 02:07:00 -0500

    Enable video recording and always upload artifacts without warnings

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = {
     defaultCommandTimeout: 10000,
     requestTimeout: 10000,
     responseTimeout: 10000,
-    video: false,
+    video: true,
     screenshotOnRunFailure: true,
     supportFile: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.cy.js',
